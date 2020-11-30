f4b40a0d4b1a0dac01e7f91682fbd8258f5e9c41
FarahNajib <farahengnajib@gmail.com>
2020-11-30 23:58:00 +0800

    working on component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,43 @@ export default class Footer extends Component {
     return (
     <div className="footer">
       <footer class="py-5 bg-dark fixed-bottom">
+
+     
+
+      <div class="container">
+
+
+<ul class="list-unstyled list-inline text-center">
+  <li class="list-inline-item">
+    <a class="btn-floating btn-fb mx-1">
+      <i class="fab fa-facebook-f"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-tw mx-1">
+      <i class="fab fa-twitter"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-gplus mx-1">
+      <i class="fab fa-google-plus-g"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-li mx-1">
+      <i class="fab fa-linkedin-in"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-dribbble mx-1">
+      <i class="fab fa-dribbble"> </i>
+    </a>
+  </li>
+</ul>
+
+
+</div>
+
         <div class="container">
           <p class="m-0 text-center text-white">
             Copyright &copy; Your Website 2020
