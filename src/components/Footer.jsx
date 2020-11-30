d66556d223c0254c82867685ce38750425847f74
FarahNajib <farahengnajib@gmail.com>
2020-11-30 23:40:24 +0800

    working on component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+
+  render() {
+    return (
+    <div className="footer">
+      <footer class="py-5 bg-dark fixed-bottom">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Your Website 2020
+          </p>
+        </div>
+      </footer>
+    </div>)
+
+  }}
