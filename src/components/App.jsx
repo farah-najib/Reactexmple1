e09ba6bbb4c8d82991482e7b1c6f4cc68cd46a98
FarahNajib <farahengnajib@gmail.com>
2020-12-02 22:30:31 +0800

    add more component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main'
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
 
 function App() {
   return (
-      <div className="App">
-   <Header/>
+    <div className="App">
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/about" exact component={() => <About />} />
+        <Route path="/contact" exact component={() => <Contact />} />
+      </Switch>
       <Footer />
-      <Main/>
-    
+    </Router>
   </div>
 );
 }
 export default App;
+
+
+
+
