3d8fdfe7004277874cde4ca05597e6d0f4186787
FarahNajib <farahengnajib@gmail.com>
2021-01-08 00:52:59 +0800

    add images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import Card from "./Card"
+
+
 const posts = [
     
   { id: 0, 
     content: {
-      title: 'Echinacea flower',
-      image: 'https://leoraw.com/images/slidein/echinacea-200px.jpg',
-      content: 'Grow some echinacea',
+      title: 'Cross Stitch',
+      image:"../images/crossstitch.jpg",
+      content: 'cross stitch example',
     },
     likeIsClicked: true,
     likes: 5 },
     { id: 1,
     content : {
-      title: 'Lake in New York State',
-      image: 'https://www.leoraw.com/images/slidein/lake-400px.jpg',
-      content: 'Worth a visit',
+      title: 'French Knote',
+      image: "../images/frenchknote.jpg",
+      content: 'french knote example',
     },
     likeIsClicked: true,
     likes: 15},
     { id: 2,
       content : {
-        title: 'Street in Jerusalem',
-        image: 'https://leoraw.com/images/slidein/jerusalem-street-400px.jpg',
-        content: 'Street in Valley of Refaim',
+        title: 'Embrodiry',
+        image: '../images/embrodiry.jpg',
+        content: 'imge',
       },
       likeIsClicked: true,
       likes: 8},
